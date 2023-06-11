d7b3315de3d19a6d4d8e5800dc9a4062774fac80
Sae Matsuda <sae.matsuda.843@gmail.com>
2023-06-11 18:33:33 +0900

    js

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const question = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
 
-question.forEach((question) => {
+
+questions.forEach((question) => {
     question.addEventListener("click", () => {
-        
+        const answerOutput = question.querySelectorAll(".answer");
+
+        answerOutput.forEach((answer) => {
+            if(answer.classList.contains("active")) {
+                answer.classList.remove("active")
+            }else{
+                answer.classList.add("active")
+            }
+        })
     })
 })
